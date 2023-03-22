e856e17440d63af726450508917f46edc12f328d
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-03-22 20:23:52 +0300

    изменила конфтест

--- web_tests/conftest.py
+++ web_tests/conftest.py
@@ -16,9 +16,4 @@ def open_browser():
     allure.attach(browser.driver.page_source, name='Page Source', attachment_type=attachment_type.HTML)
     allure.attach(("".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))), name='Log',
                   attachment_type=attachment_type.TEXT)
-    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
-    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
-           + video_url \
-           + "' type='video/mp4'></video></body></html>"
-    allure.attach(html, 'video_' + browser.driver.session_id, attachment_type.HTML, '.html')
     browser.quit()
