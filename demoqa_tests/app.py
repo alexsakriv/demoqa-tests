59947f9730fcce0d1c95d30316e1813790cf3cf2
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-03-22 20:27:52 +0300

    изменила конфтест

--- demoqa_tests/app.py
+++ demoqa_tests/app.py
@@ -1,3 +1,4 @@
 from demoqa_tests.model.pages import automation_practice_form
 from demoqa_tests.model.contols import radiobutton, datepicker, dropdown, checkbox
 from demoqa_tests.utils import uploadPicture
+from demoqa_tests.utils import attach
--- web_tests/conftest.py
+++ web_tests/conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selene.support.shared import browser
-from selene import command, have
-from allure import attachment_type
-import allure
+from demoqa_tests.utils import attach
 
 
 @pytest.fixture(autouse=True)
@@ -12,8 +10,7 @@ def open_browser():
 
     yield
 
-    allure.attach(browser.driver.get_screenshot_as_png(), name='Screenshot', attachment_type=attachment_type.JPG)
-    allure.attach(browser.driver.page_source, name='Page Source', attachment_type=attachment_type.HTML)
-    allure.attach(("".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))), name='Log',
-                  attachment_type=attachment_type.TEXT)
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
     browser.quit()
