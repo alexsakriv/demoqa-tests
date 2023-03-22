c19fe85ea61b2f7ea491ba4bc117145f28c2f206
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-03-22 20:48:39 +0300

    изменила конфтест

--- web_tests/conftest.py
+++ web_tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selene.support.shared import browser
 from demoqa_tests.utils import attach
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 
 @pytest.fixture(autouse=True)
@@ -8,9 +10,28 @@ def open_browser():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     yield
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()
